8dd49647b46b8775796ba74baab9648721c5b667
mota <tatsuya.fp@gmail.com>
2023-01-20 15:51:58 +0900

    l-headerの中のドロワーアイコンをp-drawer-iconに変更 p-drawerのHTML、CSS記載 ドロワー開閉JS記載

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,15 +1,8 @@
-// function FixedAnime() {
-//   var headerH = $('#header-js').outerHeight(true);
-//   var scroll = $(window).scrollTop();
-//   if (scroll >= headerH){//headerの高さ以上になったら
-//       $('.l-header').addClass('fixed');//fixedというクラス名を付与
-//       $('#header-js').css('margin-top','90px');
-//     }else{//それ以外は
-//       $('.l-header').removeClass('fixed');//fixedというクラス名を除去
-//       $('#header-js').css('margin-top','0');
-//     }
-// }
-
-// $(window).scroll(function () {
-//   FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
-// });
+// ドロワーメニューの開閉
+$(function() {
+  $('.p-drawer-icon').on('click', function(){
+    $('.p-drawer').toggleClass('open');
+    $(this).toggleClass('open');
+    // $('html').toggleClass('fixed');
+  });
+})
