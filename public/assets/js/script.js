b472ff53bb582b387fb1f010e3fc3cfc95703907
mota <tatsuya.fp@gmail.com>
2023-03-09 11:06:10 +0900

    予約フォームをgoogleフォームへ連動 問い合わせ完了メッセージ出るよう調整

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -6,3 +6,28 @@ $(function() {
     // $('html').toggleClass('fixed');
   });
 })
+
+// フォームの送信時
+
+$(document).ready(function () {
+
+  $('#js-form').submit(function (event) {
+    var formData = $('#js-form').serialize();
+    $.ajax({
+      url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfsJ9_sK1xE1IC4dZGq2kQmDKFGX3So9uZt3tSAFwxGFJLiEw/formResponse",
+      data: formData,
+      type: "POST",
+      dataType: "xml",
+      statusCode: {
+        0: function () {
+          $(".p-reservation-form__end-message").slideDown();
+          $("#js-form").fadeOut();
+          $(".p-reservation-form__box").fadeOut();
+          //window.location.href = "thanks.html";
+        }
+      }
+    });
+    event.preventDefault();
+  });
+
+});
